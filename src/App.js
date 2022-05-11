1685d3ded26db1747c6bc0a0db3c81f1e46d3f2d
william-789 <riberackw@gmail.com>
2022-05-11 19:11:44 +0100

    fetch with async await

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import clientPicture from './react.jpg'
 
@@ -10,7 +10,7 @@ box-sizing: border-box;
 box-shadow: 25px 25px 55px #656565;
 border-radius: 20px;
 @media (max-width: 650px){
-    border-radius: none;
+    border-radius: 0;
   }
 a {
     cursor: pointer;
@@ -26,16 +26,16 @@ padding: 20px;
 box-sizing: border-box;
 border-radius: 20px 20px 0 0;
 @media (max-width: 650px){
-    border-radius: none;
+    border-radius: 0;
   }
 img {
     border-radius: 100%;
     border: solid yellow 5px;
-    @media (max-width: 650px){
+    @media (max-width: 450px){
         width: 50vw;
-        max-width: 80px;
+        max-width: 100px;
         height: 50vw;
-        max-height: 80px;
+        max-height: 100px;
       }
 }
 `;
@@ -43,40 +43,58 @@ const InfoBox = styled.div `
 background-color: #fff;
 padding: 20px 60px;
 border-radius: 0 0 20px 20px;
+@media (max-width: 650px){
+  border-radius: 0;
+}
 `;
 function App() {
     function getRandomInt() {
-        return Math.floor(Math.random() * (26 - 1)) + 1;
-      }
-    
-    fetch("https://randomuser.me/api/")
-    .then ((res) => res.json())
-    .then(data => console.log(data))
+      return Math.floor(Math.random() * (26 - 1)) + 1;
+    }
 
-    //returns a Promise that resolves to a JavaScript object
+    const [client, setClient] = useState([])
+
+    const FetchApi = async () => {
+      try {
+        const res = await fetch("https://randomuser.me/api/");
+        //returns a Promise that resolves to a JavaScript object
+        console.log(res.ok);
+        //show errors that didn't trigger catch() 
+        const data = await res.json();
+        //uses await cause json() also returns a Promise
+        setClient(data);
+        console.log(client);
+      } catch (err) {
+        console.log('ERROR');
+        console.error(err);        
+      }};
+
+      useEffect(() => {
+        FetchApi()
+      }, [])
 
   return (
     <MainSectionBox >
         <ClientBox>
-        <img src={clientPicture} alt="Client picture" width="120" height="120"></img>
+        <img src={client.results[0].picture.large} alt="Client picture" width="120" height="120"></img>
         </ClientBox>
         <InfoBox>
+            <h1>{client.results[0].name.title}. {client.results[0].name.first} {client.results[0].name.last}</h1>
             <h1>Adress</h1>
-            <p>28173 street, city - state</p>
-            <p>postcode</p>
+            <p>{client.results[0].location.street.number} {client.results[0].location.street.name}, {client.results[0].location.city} - {client.results[0].location.state}</p>
+            <p>{client.results[0].postcode}</p>
             <h1>Contact Info</h1>
-            <a href = "mailto: abc@example.com">abc@example.com</a>
-            <p>phone: <a href="tel:123-456-7890">123-456-7890</a></p>
-            <p>cell: <a href="081-454-0668">081-454-0668</a></p>
+            <a href = "mailto: {client.results[0].email}">{client.results[0].email}</a>
+            <p>phone: <a href={client.results[0].phone}>{client.results[0].phone}</a></p>
+            <p>cell: <a href={client.results[0].cell}>{client.results[0].cell}</a></p>
             <br></br>
-            <p>Been a client for ? years</p>
+            <p>Been a client for {client.results[0].registered.age} years</p>
             <p>Coupons redeemed {getRandomInt()}</p>
         </InfoBox>
         
     </MainSectionBox>
   )
 }
-/*MISSING: Loading animation
-implement async amd await to fetch api*/
+/*MISSING: Loading animation*/
 
 export default App;
